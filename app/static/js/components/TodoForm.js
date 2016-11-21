a6c6e95f016367a1a1e8506d2020f82c866a0e9b
gm925 <gm925925@live.com>
2016-11-21 21:52:16 +0800

    init

---
+++ app/static/js/components/TodoForm.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom')
+
+var TodoForm =React.createClass({
+    handleSubmit:function (e) {
+        e.preventDefault();
+        var content = ReactDOM.findDOMNode(this.refs.content).value.trim();
+        if(!content){
+            return
+        }
+        this.props.addTodo(content);
+        ReactDOM.findDOMNode(this.refs.content).value = "";
+    },
+    render : function(){
+        return (
+            <form className="input-group" onSubmit = {this.handleSubmit}>
+                <input ref="content" className="form-control" id="content" name="content" type="text"/>
+                <span className="input-group-btn">
+                    <button className="btn btn-default" type="submit">Add</button>
+                </span>
+            </form>
+        )
+    }
+});
+
+module.exports = TodoForm;
---
+++ app/static/js/index.js
@@ -0,0 +1,6 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+var Todo = require("./components/Todo");
+
+ReactDOM.render(<Todo/>,document.getElementById("todo-container"));
+
